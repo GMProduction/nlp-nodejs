29e85f58d44c900f639f9b50a9581d81bf7c29e7
bagusyanuar <50134851+bagusyanuar@users.noreply.github.com>
2022-07-23 16:42:38 +0700

    size

--- src/controller/pdf-service.js
+++ src/controller/pdf-service.js
@@ -3,7 +3,9 @@ const fs = require('fs');
 const blobStream = require('blob-stream');
 
 function buildPDF(dataCallback, endCallback, res) {
-    const doc = new PDFDocument();
+    const doc = new PDFDocument({
+        size: [180, 297]
+    });
     doc.pipe(res);
     doc.fontSize(25).text('Here is some vector graphics...', 100, 80);
     doc.end();
